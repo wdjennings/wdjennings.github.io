2626f4389cba7f1dee6b829df9427cb44a20f800
wdj <wdj240@gmail.com>
2018-10-16 23:46:50 +0100

    Initial commit

---
+++ assets/js/video.js
@@ -0,0 +1,21 @@
+//var myVideo2 = document.getElementById("video1"); 
+
+function auto_play(video){
+    if (video.paused)
+        video.play();
+    else
+        video.pause(); 
+}
+
+var video1 = document.getElementById('video1');
+video1.addEventListener('click',function() { auto_play(video1); },false);
+
+var video2 = document.getElementById('video2');
+video2.addEventListener('click',function() { auto_play(video2); },false);
+
+var video3 = document.getElementById('video3');
+video3.addEventListener('click',function() { auto_play(video3); },false);
+
+var video4 = document.getElementById('video4');
+video4.addEventListener('click',function() { auto_play(video4); },false);
+
